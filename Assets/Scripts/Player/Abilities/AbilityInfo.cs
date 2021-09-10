62d57e054ac609ba2dd671d7be37ab1d6a11697d
JefferyYC <jefferyyc@gmail.com>
2021-09-10 11:56:41 -0700

    submission

---
+++ Assets/Scripts/Player/Abilities/AbilityInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AbilityInfo
+{
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("How much power this ability has")]
+    private int m_power;
+    public int Power
+    {
+        get
+        {
+            return m_power;
+        }
+    }
+
+    [SerializeField]
+    [Tooltip("If attack shoots something, describes the range it can shoot")]
+    private int m_range;
+    public int Range
+    {
+        get
+        {
+            return m_range;
+        }
+    }
+    #endregion
+}
